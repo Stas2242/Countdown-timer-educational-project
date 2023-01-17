7601d8cbf705b11f8f2bf1f2216f43db5c3f4a8d
Stanislav_Noks <stas2242@mail.ru>
2023-01-17 22:31:28 +0900

    Модуль 12
    - Добавлен класс Таймер
    - countDown - метод Таймера

--- index.js
+++ index.js
@@ -1,98 +1,124 @@
-'use strict';
+"use strict";
 
-const inputTitleText = document.getElementById(['title-date']);
-const mainTitle = document.querySelector('h1');
-const buttonStart = document.getElementById('btn');
-const buttonReset = document.getElementById(['btn-reset']);
-const inputDate = document.getElementById('date');
-const numbers = document.querySelector('.numbers');
+const inputTitleText = document.getElementById(["title-date"]);
+const mainTitle = document.querySelector("h1");
+const buttonStart = document.getElementById("btn");
+const buttonReset = document.getElementById(["btn-reset"]);
+const inputDate = document.getElementById("date");
+const numbers = document.querySelector(".numbers");
 
-const firstScreen = document.querySelector('.input');
-const secondScreen = document.querySelector('.output');
+const firstScreen = document.querySelector(".input");
+const secondScreen = document.querySelector(".output");
 
-let timerDeadline;
-let timerId = null;
-let dateNow = moment();
+let timer;
 
-function timerStart () {
-    localStorage.setItem('date', inputDate.value);
-    timerDeadline = inputDate.value;
+function timerStart() {
+  localStorage.setItem("date", inputDate.value);
+  timer = new Timer(inputDate.value, numbers);
+  timer.start();
 
-    if (inputDate.value === '') {
-        alert('Пожалуйста введите дату');
-        return;
-    }
+  if (timer.activation != true) {
+    return;
+  }
 
-    if (moment(timerDeadline).diff(dateNow) <= 0) {
-        inputDate.value = '';
-        alert('Дата прошла! Введите более позднюю дату!');
-        return; 
-    }
-
-    countDown();
-    timerId = setInterval(countDown, 1000);
-    titleChange();
-    newScreen();
+  titleChange();
+  newScreen();
 }
 
-function countDown () {
-    dateNow = moment(); 
+class Timer {
+  constructor(timerDeadline, numbers, dateNow, timerId, activation) {
+    this.timerDeadline = timerDeadline;
+    this.dateNow = moment();
+    this.timerId = null;
+    this.numbers = numbers;
+    this.activation = false;
+  }
 
-    if (moment(timerDeadline).diff(dateNow) <= 0) {
-        clearInterval(timerId);
-        return;
+  start() {
+    if (this.timerDeadline === '') {
+      alert('Пожалуйста введите дату');
+      return;
     }
 
-    let days = moment(timerDeadline).diff(dateNow, 'days');
-    let hours = moment(timerDeadline).diff(dateNow, 'hours') % 24;
-    let minutes = moment(timerDeadline).diff(dateNow, 'minutes') % 60;
-    let seconds = moment(timerDeadline).diff(dateNow, 'seconds') % 60;
+    if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
+      alert('Дата прошла! Введите более позднюю дату!');
+      return;
+    }
 
-    numbers.textContent = `${days < 10 ? '0' + days : days}:${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds: seconds}`    
+    this.activation = true;
+    this.countDown();
+    this.timerId = setInterval(this.countDown.bind(this), 1000);
+  }
+
+  countDown() {
+    this.dateNow = moment();
+
+    if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
+      clearInterval(this.timerId);
+      return;
+    }
+
+    let days = moment(this.timerDeadline).diff(this.dateNow, 'days');
+    let hours = moment(this.timerDeadline).diff(this.dateNow, 'hours') % 24;
+    let minutes = moment(this.timerDeadline).diff(this.dateNow, 'minutes') % 60;
+    let seconds = moment(this.timerDeadline).diff(this.dateNow, 'seconds') % 60;
+
+    this.numbers.textContent = `${days < 10 ? "0" + days : days}:${
+      hours < 10 ? "0" + hours : hours
+    }:${minutes < 10 ? "0" + minutes : minutes}:${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+  }
+
+  clearInterval() {
+    clearInterval(this.timerId);
+  }
 }
 
-function titleChange () {
-    const newTitle = `${inputTitleText.value} (${moment(timerDeadline).format('DD.MM.YYYY')})`;
-    const localTitle = localStorage.setItem('title', newTitle);
-    mainTitle.innerHTML = newTitle;
+function titleChange() {
+  const newTitle = `${inputTitleText.value} (${moment(inputDate.value).format(
+    'DD.MM.YYYY'
+  )})`;
+  const localTitle = localStorage.setItem('title', newTitle);
+  mainTitle.innerHTML = newTitle;
 }
 
-function newScreen () {
-    firstScreen.classList.add('hide');
-    secondScreen.classList.remove('hide');
-    buttonStart.classList.add('hide');
-    buttonReset.classList.remove('hide');
+function newScreen() {
+  firstScreen.classList.add('hide');
+  secondScreen.classList.remove('hide');
+  buttonStart.classList.add('hide');
+  buttonReset.classList.remove('hide');
 }
 
 function timerReset() {
-    firstScreen.classList.remove('hide');
-    secondScreen.classList.add('hide');
-    buttonStart.classList.remove('hide');
-    buttonReset.classList.add('hide');
+  firstScreen.classList.remove('hide');
+  secondScreen.classList.add('hide');
+  buttonStart.classList.remove('hide');
+  buttonReset.classList.add('hide');
 
-    mainTitle.innerHTML = 'Создать новый таймер обратного отсчета';
-    inputTitleText.value = '';
-    inputDate.value = '';
-    clearInterval(timerId);
-    
-    localStorage.removeItem('title');
-    localStorage.removeItem('date');
+  mainTitle.innerHTML = 'Создать новый таймер обратного отсчета';
+  inputTitleText.value = '';
+  inputDate.value = '';
+  timer.clearInterval();
+
+  localStorage.removeItem('title');
+  localStorage.removeItem('date');
 }
 
 const localStorageCheck = () => {
-    const localDate = localStorage.getItem('date');
-    const localTitle = localStorage.getItem('title');
+  const localDate = localStorage.getItem('date');
+  const localTitle = localStorage.getItem('title');
 
-    if (!localDate && !localTitle) {
-        return;
-    }
+  if (!localDate && !localTitle) {
+    return;
+  }
 
-    timerDeadline = localDate;
-    mainTitle.textContent = localTitle;
+  timer = new Timer(localDate, numbers);
+  timer.start();
+  mainTitle.textContent = localTitle;
 
-    timerId = setInterval(countDown, 1000);
-    newScreen();
-}
+  newScreen();
+};
 
 localStorageCheck();
 
