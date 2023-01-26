aade1565f6e1b1a66aca07a7f7194e85aa11190b
Stanislav_Noks <stas2242@mail.ru>
2023-01-26 23:03:09 +0900

    Правки по eslint

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-"use strict";
+// 'use strict';
 
-import {Timer} from './timer.js';
+import { Timer } from './timer.js';
 
-const inputTitleText = document.getElementById(["title-date"]);
-const mainTitle = document.querySelector("h1");
-const buttonStart = document.getElementById("btn");
-const buttonReset = document.getElementById(["btn-reset"]);
-const inputDate = document.getElementById("date");
-const numbers = document.querySelector(".numbers");
+const inputTitleText = document.getElementById(['title-date']);
+const mainTitle = document.querySelector('h1');
+const buttonStart = document.getElementById('btn');
+const buttonReset = document.getElementById(['btn-reset']);
+const inputDate = document.getElementById('date');
+const numbers = document.querySelector('.numbers');
 
-const firstScreen = document.querySelector(".input");
-const secondScreen = document.querySelector(".output");
+const firstScreen = document.querySelector('.input');
+const secondScreen = document.querySelector('.output');
 
 let timer;
 
 function timerStart() {
-  localStorage.setItem("date", inputDate.value);
+  localStorage.setItem('date', inputDate.value);
   timer = new Timer(inputDate.value, numbers);
   timer.start();
 
@@ -26,56 +26,6 @@ function timerStart() {
   titleChange();
   newScreen();
 }
-
-// class Timer {
-//   constructor(timerDeadline, numbers, dateNow, timerId, activation) {
-//     this.timerDeadline = timerDeadline;
-//     this.dateNow = moment();
-//     this.timerId = null;
-//     this.numbers = numbers;
-//     this.activation = false;
-//   }
-
-//   start() {
-//     if (this.timerDeadline === '') {
-//       alert('Пожалуйста введите дату');
-//       return;
-//     }
-
-//     if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
-//       alert('Дата прошла! Введите более позднюю дату!');
-//       return;
-//     }
-
-//     this.activation = true;
-//     this.countDown();
-//     this.timerId = setInterval(this.countDown.bind(this), 1000);
-//   }
-
-//   countDown() {
-//     this.dateNow = moment();
-
-//     if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
-//       clearInterval(this.timerId);
-//       return;
-//     }
-
-//     let days = moment(this.timerDeadline).diff(this.dateNow, 'days');
-//     let hours = moment(this.timerDeadline).diff(this.dateNow, 'hours') % 24;
-//     let minutes = moment(this.timerDeadline).diff(this.dateNow, 'minutes') % 60;
-//     let seconds = moment(this.timerDeadline).diff(this.dateNow, 'seconds') % 60;
-
-//     this.numbers.textContent = `${days < 10 ? "0" + days : days}:${
-//       hours < 10 ? "0" + hours : hours
-//     }:${minutes < 10 ? "0" + minutes : minutes}:${
-//       seconds < 10 ? "0" + seconds : seconds
-//     }`;
-//   }
-
-//   clearInterval() {
-//     clearInterval(this.timerId);
-//   }
-// }
 
 function titleChange() {
   const newTitle = `${inputTitleText.value} (${moment(inputDate.value).format(
--- timer.js
+++ timer.js
@@ -1,53 +1,53 @@
-'use strict';
+// 'use strict';
 
 class Timer {
-    constructor(timerDeadline, numbers, dateNow, timerId, activation) {
-      this.timerDeadline = timerDeadline;
-      this.dateNow = moment();
-      this.timerId = null;
-      this.numbers = numbers;
-      this.activation = false;
-    }
-  
-    start() {
-      if (this.timerDeadline === '') {
-        alert('Пожалуйста введите дату');
-        return;
-      }
-  
-      if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
-        alert('Дата прошла! Введите более позднюю дату!');
-        return;
-      }
-  
-      this.activation = true;
-      this.countDown();
-      this.timerId = setInterval(this.countDown.bind(this), 1000);
-    }
-  
-    countDown() {
-      this.dateNow = moment();
-  
-      if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
-        clearInterval(this.timerId);
-        return;
-      }
-  
-      let days = moment(this.timerDeadline).diff(this.dateNow, 'days');
-      let hours = moment(this.timerDeadline).diff(this.dateNow, 'hours') % 24;
-      let minutes = moment(this.timerDeadline).diff(this.dateNow, 'minutes') % 60;
-      let seconds = moment(this.timerDeadline).diff(this.dateNow, 'seconds') % 60;
-  
-      this.numbers.textContent = `${days < 10 ? "0" + days : days}:${
-        hours < 10 ? "0" + hours : hours
-      }:${minutes < 10 ? "0" + minutes : minutes}:${
-        seconds < 10 ? "0" + seconds : seconds
-      }`;
-    }
-  
-    clearInterval() {
-      clearInterval(this.timerId);
-    }
+  constructor(timerDeadline, numbers) {
+    this.timerDeadline = timerDeadline;
+    this.dateNow = moment();
+    this.timerId = null;
+    this.numbers = numbers;
+    this.activation = false;
   }
 
-  export {Timer};
+  start() {
+    if (this.timerDeadline === '') {
+      alert('Пожалуйста введите дату');
+      return;
+    }
+
+    if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
+      alert('Дата прошла! Введите более позднюю дату!');
+      return;
+    }
+
+    this.activation = true;
+    this.countDown();
+    this.timerId = setInterval(this.countDown.bind(this), 1000);
+  }
+
+  countDown() {
+    this.dateNow = moment();
+
+    if (moment(this.timerDeadline).diff(this.dateNow) <= 0) {
+      clearInterval(this.timerId);
+      return;
+    }
+
+    const days = moment(this.timerDeadline).diff(this.dateNow, 'days');
+    const hours = moment(this.timerDeadline).diff(this.dateNow, 'hours') % 24;
+    const minutes = moment(this.timerDeadline).diff(this.dateNow, 'minutes') % 60;
+    const seconds = moment(this.timerDeadline).diff(this.dateNow, 'seconds') % 60;
+
+    this.numbers.textContent = `${days < 10 ? '0' + days : days}:${
+      hours < 10 ? '0' + hours : hours
+    }:${minutes < 10 ? '0' + minutes : minutes}:${
+      seconds < 10 ? '0' + seconds : seconds
+    }`;
+  }
+
+  clearInterval() {
+    clearInterval(this.timerId);
+  }
+}
+
+export { Timer };
